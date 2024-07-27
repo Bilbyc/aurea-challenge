82420199e4ade5d0199a70dddcf33a4683db5137
Carlos Bilby <carlos.bilby@gmail.com>
2024-07-27 13:42:19 -0300

    feat: adiciona modules de controller e service ao app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app/infrastructure/controllers/app.controller';
 import { AppService } from './app/services/app.service';
+import { ControllersModule } from './app/infrastructure/controllers/controllers.module';
+import { ServicesModule } from './app/services/sevices.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ControllersModule, ServicesModule],
+  providers: [],
 })
 export class AppModule {}
